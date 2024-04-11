9fc88acd8bab131ef53e039c087971299fd13a22
Success <successsimeon484@gmail.com>
2024-04-11 22:07:22 +0100

    initial commit

--- full_Auth/settings.py
+++ full_Auth/settings.py
@@ -196,7 +196,7 @@ DOMAIN = getenv('DOMAIN')
 SITE_NAME = 'FULL AUTH'
 
 
-CORS_ALLOWED_ORIGINS = getenv('CORS_ALLOWED_ORIGINS', 'http://localhost:3000,http://127.0.0.1:3000').split(',')
+CORS_ALLOWED_ORIGINS = getenv('CORS_ALLOWED_ORIGINS', 'http://localhost:3000,http://127.0.0.1:3000,https://full-auth-1.onrender.com').split(',')
 CORS_ALLOW_CREDENTIALS = True
 
 
