035bfbc7eab9b183942294bb64654545ece050e9
Success <successsimeon484@gmail.com>
2024-04-15 00:02:40 +0100

    Your commit message here

--- full_Auth/settings.py
+++ full_Auth/settings.py
@@ -95,11 +95,14 @@ WSGI_APPLICATION = 'full_Auth.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Odion12345$$',
+        'HOST': 'localhost', 
+        'PORT': '5432',       
     }
 }
-
 
 
 # Password validation
