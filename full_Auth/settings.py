ba278b30ccb1ee9c95f81944d95dd6ce66efc693
Success <successsimeon484@gmail.com>
2024-04-24 14:18:21 +0100

    first commit

--- full_Auth/settings.py
+++ full_Auth/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = getenv('DJANGO_SECRET_KEY', get_random_secret_key())
 DEBUG = True
 
 
-ALLOWED_HOSTS = getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost,full-auth-1.onrender.com").split(",")
+ALLOWED_HOSTS = getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost,full-auth-b0gw.onrender.com").split(",")
 
 # Application definition
 
